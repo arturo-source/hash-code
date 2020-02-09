97e6949d92c105bca4d7260eb40a77a5e74f7661
arturo-source <arturo.com>
2020-02-09 14:03:38 +0100

    Read arguments added

--- pizza-2020/main.go
+++ pizza-2020/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
+
+//Greedy(maxSlices uint32, pizzasIn []uint32) (pizzasOut []uint32, amount uint32)
 
 func outputFile(filePath string, data []uint32) error {
 	var fileData string
@@ -16,10 +19,20 @@ func outputFile(filePath string, data []uint32) error {
 }
 
 func main() {
-	filePath := "output/small.out"
-	data := []uint32{10, 20, 2, 3}
+	if len(os.Args) < 3 {
+		panic("There isn't enought arguments")
+	}
+	filePathIn := os.Args[1]
+	filePathOut := os.Args[2]
 
-	err := outputFile(filePath, data)
+	maxSlices, typeSlices, err := read(filePathIn)
+	if err != nil {
+		panic(err)
+	}
+
+	pizzasOut, _ := Greedy(maxSlices, typeSlices)
+
+	err = outputFile(filePathOut, pizzasOut)
 	if err != nil {
 		panic(err)
 	}
