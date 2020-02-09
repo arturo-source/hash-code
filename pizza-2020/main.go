36f55390d3ec4fccc4a7c08fa3712a3f2a2fe1a6
arturo-source <arturo.com>
2020-02-09 13:50:27 +0100

    Output format added

--- pizza-2020/main.go
+++ pizza-2020/main.go
@@ -1,14 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func outputFile(filePath string, data []uint32) error {
+	var fileData string
+	fileData = fmt.Sprintf("%d\n%v", len(data), strings.Trim(fmt.Sprint(data), "[]"))
+
+	ioutil.WriteFile(filePath, []byte(fileData), 0644)
+
+	return nil
+}
 
 func main() {
-	filePath := "input/e_also_big.in"
-	maxSlices, typeSlices, err := read(filePath)
+	filePath := "output/small.out"
+	data := []uint32{10, 20, 2, 3}
+
+	err := outputFile(filePath, data)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("Max Slices: ", maxSlices)
-	fmt.Println("Content of array: ", typeSlices)
 }
