9eb4acc5b78861a31789022fe737a18701e64298
Cayetano Manchon <cmp102@alu.ua.es>
2020-02-15 18:51:29 +0100

    New Optimistic and compare

--- pizza-2020/tnode.go
+++ pizza-2020/tnode.go
@@ -1,14 +1,50 @@
 package main
 
+import (
+	"github.com/Workiva/go-datastructures/queue"
+)
+
 type TNode struct {
 	Pizzas     []uint32
 	Pos        uint16
 	Deep       uint16
-	amount     uint32
+	Amount     uint32
 	Optimistic uint32
 }
 
+func (tnode *TNode) Compare(queue.Item) int {
+	return 0
+}
+
+func (tnode *TNode) setOptimistic(maxSlices uint32, pizzasIn []uint32) {
+	tnode.Optimistic = tnode.Amount
+	for i := tnode.Pos; i < uint16(len(pizzasIn)); i++ {
+		slices := pizzasIn[i]
+		if tnode.Optimistic+slices > maxSlices {
+			return
+		}
+		tnode.Optimistic += slices
+	}
+	return
+}
+
+func NewNode(maxSlices uint32, pizzasIn []uint32) (tnode *TNode) {
+	if len(pizzasIn) > 0 {
+		tnode = &TNode{
+			[]uint32{0},
+			0,
+			0,
+			0,
+			0}
+	}
+	tnode.setOptimistic(maxSlices, pizzasIn)
+	return
+}
+
 func (tnode *TNode) Expand(pizzasIn []uint32) (sons []TNode) {
+	for i := tnode.Pos + 1; i < uint16(len(pizzasIn)); i++ {
+
+	}
 	return
 }
 
