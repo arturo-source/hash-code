563ceeb37efabbdfc6f2e6287105bd17d425992b
Antonio <aml125@alu.ua.es>
2020-02-15 20:03:40 +0100

    algorithm branch and bound

--- pizza-2020/algorithms.go
+++ pizza-2020/algorithms.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/Workiva/go-datastructures/queue"
+
 //Greedy gets the fastes result
 func Greedy(maxSlices uint32, pizzasIn []uint32) (pizzasOut []uint32, amount uint32) {
 	amount = 0
@@ -33,5 +35,53 @@ func Recursive(maxSlices uint32, pos uint32, pizzasIn []uint32) (pizzasOut []uin
 			pizzasOut = append(tmpPizzas, i)
 		}
 	}
+	return
+}
+
+//BranchBound fuck you go compiler
+func BranchBound(maxSlices uint32, pizzasIn []uint32) (pizzasOut []uint32, amount uint32) {
+	/*
+		type TNode struct {
+		Pizzas     []uint32
+		Pos        uint16
+		Deep       uint16
+		amount     uint32
+		Optimistic uint32
+		}
+	*/
+	pizzasOut, amount = Greedy(maxSlices, pizzasIn)
+	initialNode := NewNode(maxSlices, pizzasIn)
+	var bestNode *TNode
+	pizzaQueue := queue.NewPriorityQueue(0, true)
+
+	pizzaQueue.Put(initialNode)
+
+	for !pizzaQueue.Empty() {
+		auxn, _ := pizzaQueue.Get(1)
+
+		nod := auxn[0].(*TNode)
+
+		// if true /*nod.Optimistic > amount*/ {
+		if nod.Pessimistic > amount {
+			amount = nod.Pessimistic
+		}
+
+		if nod.Pessimistic == nod.Amount && nod.Amount >= amount {
+			bestNode = nod
+		}
+
+		for _, newNod := range nod.Expand(pizzasIn) {
+			if newNod.Amount >= amount {
+				pizzaQueue.Put(&newNod)
+			}
+		}
+		// }
+	}
+
+	if bestNode != nil {
+		amount = bestNode.Amount
+		pizzasOut = bestNode.Pizzas
+	}
+
 	return
 }
