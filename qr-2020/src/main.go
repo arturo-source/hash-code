a30a0a4bea8a8eaaf7d8f88a22e9bcd9329d15ab
Antonio <aml125@alu.ua.es>
2020-02-20 17:52:09 +0100

    read file added

--- qr-2020/src/main.go
+++ qr-2020/src/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+var maxSize int
+var totalTypes int
+var types []int
+var solution []int
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
 	fmt.Println("Hello HashCode!")
+}
+
+func readFile(fName string) {
+	dat, err := ioutil.ReadFile(fName)
+	check(err)
+
+	fileLines := strings.Split(string(dat), "\n")
+
+	aux := strings.Split(fileLines[0], " ")
+	maxSize, _ = strconv.Atoi(aux[0])
+	totalTypes, _ = strconv.Atoi(aux[1])
+
+	types = make([]int, totalTypes)
+	for i, val := range strings.Split(fileLines[1], " ") {
+		types[i], _ = strconv.Atoi(val)
+	}
+
+	fmt.Println("types", types)
+	fmt.Println("maxSize", maxSize)
+	fmt.Println("totalTypes", totalTypes)
 }
