6952f2c1b42519f35ca909d26dd1ef5eecf1e618
Antonio <aml125@alu.ua.es>
2020-02-20 19:53:27 +0100

    file read

--- qr-2020/src/main.go
+++ qr-2020/src/main.go
@@ -8,11 +8,6 @@ import (
 	"strings"
 )
 
-var maxSize int
-var totalTypes int
-var types []int
-var solution []int
-
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -27,24 +22,57 @@ func main() {
 	filePathIn := os.Args[1]
 	filePathOut := os.Args[2]
 	fmt.Println(filePathIn, filePathOut)
+	readFile(filePathIn)
 }
 
 func readFile(fName string) {
 	dat, err := ioutil.ReadFile(fName)
 	check(err)
 
+	//Read total Books Libraries and Days to scan
 	fileLines := strings.Split(string(dat), "\n")
 
 	aux := strings.Split(fileLines[0], " ")
-	maxSize, _ = strconv.Atoi(aux[0])
-	totalTypes, _ = strconv.Atoi(aux[1])
+	totalBooks, _ := strconv.Atoi(aux[0])
+	totalLibs, _ := strconv.Atoi(aux[1])
+	auxTime, _ := strconv.Atoi(aux[2])
+	Time = uint32(auxTime)
 
-	types = make([]int, totalTypes)
-	for i, val := range strings.Split(fileLines[1], " ") {
-		types[i], _ = strconv.Atoi(val)
+	//Read books
+	Books = make([]uint32, totalBooks)
+	auxBooks := strings.Split(fileLines[1], " ")
+	for i := 0; i < totalBooks; i++ {
+		auxValue, _ := strconv.Atoi(auxBooks[i])
+		Books[i] = uint32(auxValue)
 	}
 
-	fmt.Println("types", types)
-	fmt.Println("maxSize", maxSize)
-	fmt.Println("totalTypes", totalTypes)
+	//Read Libraries
+	Libraries = make([]LibraryT, totalLibs)
+	length := totalLibs * 2
+	for i := 2; i <= length; i += 2 {
+		//Read library initial data
+		auxLibs := strings.Split(fileLines[i], " ")
+		var lib LibraryT
+
+		totalBooksLib, _ := strconv.Atoi(auxLibs[0])
+		singupTime, _ := strconv.Atoi(auxLibs[1])
+		shipLimit, _ := strconv.Atoi(auxLibs[2])
+
+		lib.Books = make([]uint32, totalBooksLib)
+		lib.SignTime = uint32(singupTime)
+		lib.DailyBooks = uint32(shipLimit)
+
+		//Read library books information
+		for j, val := range strings.Split(fileLines[i+1], " ") {
+			auxValue, _ := strconv.Atoi(val)
+			lib.Books[j] = uint32(auxValue)
+		}
+		Libraries[i/2-1] = lib
+	}
+
+	fmt.Println("Days: ", Time)
+	fmt.Println("Books: ", totalBooks)
+	fmt.Println(Books)
+	fmt.Println("Libraries: ", totalLibs)
+	fmt.Println(Libraries)
 }
